b783933f2f02b8d55e6f199432e639a379f736bb
Mahmudulhasansiyam11 <mahmudulsiyam766@gmail.com>
2025-10-07 04:38:42 -0700

    store wishlist books to Local Storage and show toast alert

--- src/Pages/BookDetails/BookDetails.jsx
+++ src/Pages/BookDetails/BookDetails.jsx
@@ -1,4 +1,5 @@
 import { useLoaderData, useParams } from "react-router";
+import { addToStoreDB } from "../../Utility/addToDB";
 
 const BookDetails = () => {
   const { id } = useParams();
@@ -17,8 +18,13 @@ const BookDetails = () => {
     yearOfPublishing,
     rating,
   } = singleBook;
+
+  const handleMarkAsRead = (id) => {
+    addToStoreDB(id);
+  }
+
   return (
-    <div className="flex items-center gap-12 m-3">
+    <div className="md:flex items-center gap-12 m-3">
       <div className="p-[73px] bg-gray-200 rounded-2xl flex-1">
         <img src={image} alt="" />
       </div>
@@ -26,30 +32,43 @@ const BookDetails = () => {
         <h3 className="text-4xl font-bold">{bookName}</h3>
         <p className="text-[20px] font-medium">By: {author}</p>
         <div className="border border-gray-200 mt-5"></div>
-        <div className="mt-2">
-            {
-            tags.find(tag=>tag)
-        }
-        </div>
+        <div className="mt-2">{tags.find((tag) => tag)}</div>
         <div className="border border-gray-200 mt-2"></div>
         <div className="mt-5">
-            <p><span className="font-medium">review:</span>{review}</p>
+          <p>
+            <span className="font-medium">review:</span>
+            {review}
+          </p>
         </div>
         <div className="mt-5">
-            <p className="flex gap-5"><span className="font-medium">Tags: </span>
+          <p className="flex gap-5">
+            <span className="font-medium">Tags: </span>
             <div className="flex gap-3">
-                {
-                    tags.map(tag => <p className="text-[#90EE90]">{tag}</p>)
-                }
+              {tags.map((tag) => (
+                <p className="text-[#90EE90]">{tag}</p>
+              ))}
             </div>
-            </p>
+          </p>
         </div>
         <div className="border border-gray-200 mt-5"></div>
         <div className="mt-5">
-            <p>Number of Pages: <span className="font-medium">{totalPages}</span></p>
-            <p>Publisher: <span className="font-medium">{publisher}</span></p>
-            <p>Year of Publishing: <span className="font-medium">{yearOfPublishing}</span></p>
-            <p>Rating: <span className="font-medium">{rating}</span></p>
+          <p>
+            Number of Pages: <span className="font-medium">{totalPages}</span>
+          </p>
+          <p>
+            Publisher: <span className="font-medium">{publisher}</span>
+          </p>
+          <p>
+            Year of Publishing:{" "}
+            <span className="font-medium">{yearOfPublishing}</span>
+          </p>
+          <p>
+            Rating: <span className="font-medium">{rating}</span>
+          </p>
+        </div>
+        <div className="mt-5">
+        <button onClick={() => handleMarkAsRead(id)} class="btn btn-accent m-3">Mark as Read</button>
+        <button class="btn btn-info m-3">Add to WishList</button>
         </div>
       </div>
     </div>
