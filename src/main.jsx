7c91f4ac83df8fe82d199e44b75f25e05fff79e1
Mahmudulhasansiyam11 <mahmudulsiyam766@gmail.com>
2025-10-07 00:18:43 -0700

    create navbar for boi poka

--- src/main.jsx
+++ src/main.jsx
@@ -1,15 +1,11 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
-import { createBrowserRouter } from "react-router";
+
 import { RouterProvider } from "react-router/dom";
 import './index.css';
+import { router } from './Routes/routes';
 
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <div className='text-center m-10'>Welcome to my site</div>,
-  },
-]);
+
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
